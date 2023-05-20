d746417a86cc355fc2d49191d666e9c8f29d7c0b
Wolfprincess <77727966+JasminZwick@users.noreply.github.com>
2023-05-20 21:25:51 +0200

    dht11 files and methods added
    added dht11.h, dht11.c
    added dht11 methods for initialization and data reading  in sensors.c

--- src/sensors/sensors.c
+++ src/sensors/sensors.c
@@ -1,16 +1,19 @@
 #include "driver/gpio.h"
 #include "sensors/bh1750.h"
+#include "sensors/dht11.h"
 
 #define LED_PIN GPIO_NUM_26
+#define DHT11_PIN GPIO_NUM_21
 
 float BRIGHTNESS = 1234.56f;
-float TEMPERATURE = 25.5f;
-float HUMIDITY = 30.00f;
+float TEMPERATURE = 0.0f;
+float HUMIDITY = 0.0f;
 
 int s_led_state = 0;
 
 void init_sensors() {
     i2c_master_init();
+    DHT11_init(DHT11_PIN);
 }
 
 void read_allSensor_Data() {
@@ -21,7 +24,19 @@ void read_allSensor_Data() {
     gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);        
     gpio_set_level(LED_PIN, s_led_state);
 
+    // read_dht();
+
     BRIGHTNESS = get_brightness();
-    TEMPERATURE += 0.01f;
-    HUMIDITY += 0.1f;
+
+    // TEMPERATURE = get_temperature();
+    TEMPERATURE = DHT11_read().temperature;
+    // HUMIDITY = get_humidity();
+    HUMIDITY = DHT11_read().humidity;
+}
+void read_dht() {
+
+
+    printf("Temperature is %.1f°C \n", DHT11_read().temperature);
+    printf("Humidity is %.1f%%\n", DHT11_read().humidity);
+    printf("Status code is %d\n", DHT11_read().status);
 }
